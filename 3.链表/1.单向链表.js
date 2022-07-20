ca955a2b26a2435de71d259d95bca2960bd6aee9
Wkb2317 <2576267399@qq.com>
2022-07-20 12:36:34 +0800

    feat: 单向链表封装

--- 3.链表/1.单向链表.js
+++ 3.链表/1.单向链表.js
@@ -73,8 +73,107 @@ class LinkNode {
         // 插入
         node.next = current;
         previous.next = node;
+        this.length += 1;
       }
     }
+  }
+
+  // 4. get
+  get(position) {
+    if (position < 0 || position >= this.length) {
+      return false;
+    }
+
+    let current = this.head;
+    let index = 0;
+    while (index < position) {
+      current = current.next;
+      index += 1;
+    }
+    return current.val;
+  }
+
+  // 5.indexOf(val)
+  indexOf(val) {
+    let current = this.head;
+    let index = 0;
+    while (index < this.length) {
+      if (current.val === val) {
+        return index;
+      } else {
+        current = current.next;
+        index++;
+      }
+    }
+
+    return -1;
+  }
+  // 6.update(position,val)
+  update(position, val) {
+    if (position < 0 || position >= this.length) {
+      return false;
+    }
+
+    let current = this.head;
+    let index = 0;
+    while (index < position) {
+      current = current.next;
+      index += 1;
+    }
+    current.val = val;
+  }
+  // 7.removeAt(position)
+  removeAt(position) {
+    if (position < 0 || position >= this.length) {
+      return false;
+    }
+    let current = this.head;
+    let previous = null;
+    let index = 0;
+    if (position === 0) {
+      this.head = this.head.next;
+    } else {
+      while (index < position) {
+        previous = current;
+        current = current.next;
+        index += 1;
+      }
+      // 移除
+      previous.next = current.next;
+      this.length -= 1;
+    }
+
+    return current.val;
+  }
+  // 8.remove(val)
+  remove(val) {
+    let current = this.head;
+    let previous = null;
+    while (current) {
+      if (current.val === val) {
+        this.length -= 1;
+        // 如果删除的不是第一个元素
+        if (previous) {
+          previous.next = current.next;
+        } else {
+          // 删除的是第一个元素
+          this.head = current.next;
+        }
+
+        return true;
+      }
+      previous = current;
+      current = current.next;
+    }
+    return false;
+  }
+  // 9.isEmpty()
+  isEmpty() {
+    return this.length === 0;
+  }
+  // 10. size()
+  size() {
+    return this.length;
   }
 }
 
@@ -84,3 +183,29 @@ links.append(3);
 links.append(2);
 links.insert(3, 10);
 console.log(links.toString());
+
+// get测试
+// console.log(links.get(2));
+// console.log(links.get(3));
+
+// indexof测试
+// console.log(links.indexOf(10));
+// console.log(links.indexOf(3));
+
+// update测试
+// links.update(3, 11);
+// links.update(1, 6);
+// console.log(links.toString());
+
+// removeAt测试
+// console.log(links.removeAt(0));
+// console.log(links.removeAt(2));
+// console.log(links.toString());
+
+// remove 测试
+// console.log(links.remove(1));
+// console.log(links.toString());
+
+// isempty,size测试
+console.log(links.isEmpty());
+console.log(links.size());
